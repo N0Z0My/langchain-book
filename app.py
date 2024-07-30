458369550a32a48c38f87d2043dabe730ab94796
N0Z0My <kutushitapengin3600@gmail.com>
2024-07-30 05:16:43 +0000

    Resolve merge conflict

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 
 import streamlit as st
-<<<<<<< HEAD
 from dotenv import load_dotenv
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.callbacks import StreamlitCallbackHandler
@@ -11,7 +10,6 @@ from langchain.prompts import MessagesPlaceholder
 
 load_dotenv()
 
-st.title("langchain-streamlit-app")
 
 def create_agent_chain():
     chat = ChatOpenAI(
@@ -34,9 +32,12 @@ def create_agent_chain():
         memory=memory,
     )
 
-    if "agent_chain" not in st.session_state:
-        st.session_state.agent_chain = create_agent_chain()
 
+if "agent_chain" not in st.session_state:
+    st.session_state.agent_chain = create_agent_chain()
+
+
+st.title("langchain-streamlit-app")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -44,34 +45,18 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
 prompt = st.chat_input("What is up?")
-print(prompt)
 
-if prompt: #入力の文字列（Noneでもから文字列でもない）がある場合
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-        with st.chat_message("assistant"):
-            chat = ChatOpenAI(
-                model_name=os.environ["OPENAI_API_MODEL"],
-                temperature=os.environ["OPENAI_API_TEMPERATURE"],
-                )
-                messages = [HumanMessage(content=prompt)]
-                response = chat(messages)
-                st.markdown(response.content)
-
-        with st.chat_message("assistant"):
+    with st.chat_message("assistant"):
         callback = StreamlitCallbackHandler(st.container())
         response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
         st.markdown(response)
 
-        st.session_state.messages.append({"role": "assistant", "content": response})
-=======
-
-st.title("langchain-streamlit-app")
-
-prompt = st.chat_input("What is up?")
-print(prompt)
->>>>>>> 008dc1ddb7e5f76966a410d249a5ae8edb8a2618
+    st.session_state.messages.append({"role": "assistant", "content": response})
